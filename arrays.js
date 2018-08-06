7374a6118ec5b19715aa3b31c2fed7f3c0386080
Bethany Mai <bethany.mai@shopko.com>
2018-08-06 17:34:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,16 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ....array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array2, element2) {
+  array2.unshift(element2)
+  return array2;
+}
+
+function addElementToEndOfArray(array3, element3) {
+  return [...array3, element3];
+}
+
+function destructivelyAddElementToEndOfArray
